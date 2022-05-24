3d4a3cf933caf93f8f9e21a8bb7f4e0ef2ca042f
Aarushnautiyal <sandeepnautiyal1115@gmail.com>
2022-05-24 13:05:37 +0530

    Learned Variables and Input Output

---
+++ inputoutput/InputOutput.java
@@ -0,0 +1,14 @@
+package inputoutput;
+
+import java.util.Scanner;
+
+public class InputOutput {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // For getting one input example: HEY we use sc.next()
+        // String name = sc.next()
+        // For getting more then one input expamle: HEY THERE we use nextLine()
+        String name = sc.nextLine();
+        System.out.println(name);
+    }
+}
